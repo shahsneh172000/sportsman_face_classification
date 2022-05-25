1aa7cf670c4ca87fe5995b18a55a80c94ba975c8
SnehShah <shahsneh172000@gmail.com>
2022-05-25 15:30:31 +0530

    My first File

---
+++ server/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+
+@app.route('/classify_image', methods=['POST'])
+def classify_image():
+    image_data = request.form['image_data']
+
+    response = jsonify(util.classify_image(image_data))
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
